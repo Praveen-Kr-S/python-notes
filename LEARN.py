3b63617f993d876f3a7091daf197456d685ea3e5
PraveenKumar S <praveenkrsslm69@gmail.com>
2026-06-04 20:28:16 +0530

    Add files via upload

---
+++ LEARN.py
@@ -0,0 +1,173 @@
+#variable
+"""
+name = "Praveen"
+print(name,type(name))
+phone = 987654321
+print(phone,type(phone))
+cgpa = 8.7
+print(cgpa,type(cgpa))
+check = False
+print(check,type(check))
+"""
+#Operators
+
+#Arithmetic operator
+#+,-,*,/,%,//,**
+'''
+a = 5
+b = 2
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a%b)
+print(a//b)
+print(a**b)  
+print(a**3) 
+'''
+
+#Comparision Operator
+#==,!=,>=,<=,>,<
+'''
+a = 10
+b = 20
+c = 10
+d = 30
+print(a == c)
+print(d == c)
+print(a != b)
+print(a < b)
+print(a > b)
+print(a <= c)
+print(d >= b)
+'''
+
+#conditional statements
+"""
+1.if-else
+
+
+if condition:
+    #True Statement
+elif condtion:
+    #true Statement
+else:
+    #False Statement   
+"""
+'''
+b = 0
+if b == 0:
+    print("b is zero...")
+elif b > 0:
+    print("B is positive")
+else:
+    print("b is Negative...")
+'''
+
+
+#Core data-type
+
+#1.List -> []
+#2.set -> {}
+#3.tuple -> ()
+#4.Dictionary -> {key:value}
+
+"""
+l = ["Praveen","Python",40,55.6,True,40]
+print(l)
+print(type(l))
+
+print(l[1])
+print(l[4])
+"""
+
+#dictionary
+
+p = {"Brand":"Samsung",
+     "Model":"Samsung S26",
+     "Ram":8,
+     "Rom":512,
+     "Price":125000}
+
+"""
+print(p)
+print(p["Brand"])
+print(p["Model"])
+print(p["Price"])
+print(p.keys())
+print(p.values())
+"""
+
+for i in p:
+    print(i,":",p[i])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
